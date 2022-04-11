3fbc27692d372dc08acad3649fb14950ed42e733
nikanike3 <nikanike36@gmail.com>
2022-04-11 15:07:14 -0700

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace oop_lab12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.addFiles = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.archiveName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.encryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.decryptToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.archiveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.unzipToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(30, 44);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(717, 136);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // addFiles
+            // 
+            this.addFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.addFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addFiles.Location = new System.Drawing.Point(392, 219);
+            this.addFiles.Name = "addFiles";
+            this.addFiles.Size = new System.Drawing.Size(161, 47);
+            this.addFiles.TabIndex = 1;
+            this.addFiles.Text = "Добавити файли";
+            this.addFiles.UseVisualStyleBackColor = true;
+            this.addFiles.Click += new System.EventHandler(this.addFiles_Click);
+            // 
+            // clear
+            // 
+            this.clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(576, 219);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(161, 46);
+            this.clear.TabIndex = 2;
+            this.clear.Text = "Очистити";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // archiveName
+            // 
+            this.archiveName.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.archiveName.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.archiveName.Location = new System.Drawing.Point(163, 229);
+            this.archiveName.Name = "archiveName";
+            this.archiveName.Size = new System.Drawing.Size(208, 29);
+            this.archiveName.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 227);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Назва архіва:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.encryptToolStripMenuItem,
+            this.decryptToolStripMenuItem,
+            this.archiveToolStripMenuItem,
+            this.unzipToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(771, 33);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // encryptToolStripMenuItem
+            // 
+            this.encryptToolStripMenuItem.Name = "encryptToolStripMenuItem";
+            this.encryptToolStripMenuItem.Size = new System.Drawing.Size(125, 29);
+            this.encryptToolStripMenuItem.Text = "Шифрувати";
+            this.encryptToolStripMenuItem.Click += new System.EventHandler(this.encryptToolStripMenuItem_Click);
+            // 
+            // decryptToolStripMenuItem
+            // 
+            this.decryptToolStripMenuItem.Name = "decryptToolStripMenuItem";
+            this.decryptToolStripMenuItem.Size = new System.Drawing.Size(152, 29);
+            this.decryptToolStripMenuItem.Text = "Розшифрувати";
+            this.decryptToolStripMenuItem.Click += new System.EventHandler(this.decryptToolStripMenuItem_Click);
+            // 
+            // archiveToolStripMenuItem
+            // 
+            this.archiveToolStripMenuItem.Name = "archiveToolStripMenuItem";
+            this.archiveToolStripMenuItem.Size = new System.Drawing.Size(119, 29);
+            this.archiveToolStripMenuItem.Text = "Архівувати";
+            this.archiveToolStripMenuItem.Click += new System.EventHandler(this.archiveToolStripMenuItem_Click);
+            // 
+            // unzipToolStripMenuItem
+            // 
+            this.unzipToolStripMenuItem.Name = "unzipToolStripMenuItem";
+            this.unzipToolStripMenuItem.Size = new System.Drawing.Size(147, 29);
+            this.unzipToolStripMenuItem.Text = "Розархівувати";
+            this.unzipToolStripMenuItem.Click += new System.EventHandler(this.unzipToolStripMenuItem1_Click);
+            // 
+            // splitter1
+            // 
+            this.splitter1.Location = new System.Drawing.Point(0, 33);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(3, 252);
+            this.splitter1.TabIndex = 10;
+            this.splitter1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 285);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.archiveName);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.addFiles);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(787, 324);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button addFiles;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.TextBox archiveName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem encryptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem decryptToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem archiveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem unzipToolStripMenuItem;
+        private System.Windows.Forms.Splitter splitter1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Compression;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace oop_lab12
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        List<string> selectedFiles = new List<string>();
+
+        OpenFileDialog openFileDialog = new OpenFileDialog();
+
+        const string password = "1234512345678976";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog.Multiselect = true;
+
+            richTextBox1.DragEnter += new DragEventHandler(richTextBox1_DragOver);
+            richTextBox1.DragDrop += new DragEventHandler(richTextBox1_DragDrop);
+            richTextBox1.AllowDrop = true;
+        }
+
+        private void addFiles_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            foreach (string file in openFileDialog.FileNames)
+            {
+                selectedFiles.Add(file);
+            }
+            selectedFiles.ForEach(file =>
+            {
+                richTextBox1.Text += file + "\n";
+            });
+            MessageBox.Show("Успішно добавлено!");
+        }
+
+        private void richTextBox1_DragOver(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void richTextBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) == true)
+            {
+                string[] fileList = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+
+                foreach (string file in fileList)
+                {
+                    selectedFiles.Add(file);
+                    richTextBox1.Text += file + "\n";
+                }
+                MessageBox.Show("Успішно добавлено!");
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            archiveName.Text = "";
+            selectedFiles.Clear();
+        }
+
+        private static void EncryptFile(string inputFile, string outputFile, string skey)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    using (FileStream fsCrypt = new FileStream(outputFile, FileMode.Create))
+                    {
+                        using (ICryptoTransform encryptor = aes.CreateEncryptor(key, IV))
+                        {
+                            using (CryptoStream cs = new CryptoStream(fsCrypt, encryptor, CryptoStreamMode.Write))
+                            {
+                                using (FileStream fsIn = new FileStream(inputFile, FileMode.Open))
+                                {
+                                    int data;
+                                    while ((data = fsIn.ReadByte()) != -1)
+                                    {
+                                        cs.WriteByte((byte)data);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static void DecryptFile(string inputFile, string outputFile, string skey)
+        {
+            try
+            {
+                using (RijndaelManaged aes = new RijndaelManaged())
+                {
+                    byte[] key = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    byte[] IV = ASCIIEncoding.UTF8.GetBytes(skey);
+
+                    using (FileStream fsCrypt = new FileStream(inputFile, FileMode.Open))
+                    {
+                        using (FileStream fsOut = new FileStream(outputFile, FileMode.Create))
+                        {
+                            using (ICryptoTransform decryptor = aes.CreateDecryptor(key, IV))
+                            {
+                                using (CryptoStream cs = new CryptoStream(fsCrypt, decryptor, CryptoStreamMode.Read))
+                                {
+                                    int data;
+                                    while ((data = cs.ReadByte()) != -1)
+                                    {
+                                        fsOut.WriteByte((byte)data);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void encryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+            commonOpenFileDialog.IsFolderPicker = true;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                EncryptFile(openFileDialog.FileName, commonOpenFileDialog.FileName + "\\" + System.IO.Path.GetFileName(openFileDialog.FileName) + ".crypt", password);
+                MessageBox.Show("Успішно зашифрований файл");
+            }
+        }
+
+        private void decryptToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+            commonOpenFileDialog.IsFolderPicker = true;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Crypted Files|*.crypt";
+            openFileDialog.Multiselect = false;
+
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                string decryptedFilePath = commonOpenFileDialog.FileName + "\\" + System.IO.Path.GetFileName(openFileDialog.FileName);
+                decryptedFilePath = decryptedFilePath.Remove(decryptedFilePath.Length - 6);
+                DecryptFile(openFileDialog.FileName, decryptedFilePath, password);
+                MessageBox.Show("Успішно розшифрований файл");
+            }
+        }
+
+        private void archiveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+            commonOpenFileDialog.IsFolderPicker = true;
+
+            if (archiveName.Text.Length >= 1 && richTextBox1.Text.Length >= 1)
+            {
+                if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    using (ZipArchive zip = ZipFile.Open(commonOpenFileDialog.FileName + "\\" + archiveName.Text + ".gzar", ZipArchiveMode.Create))
+                    {
+                        for (int i = 0; i < selectedFiles.Count; i++)
+                        {
+                            zip.CreateEntryFromFile(selectedFiles[i], System.IO.Path.GetFileName(selectedFiles[i]));
+                        }
+
+                        MessageBox.Show("Успішно створений архів");
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Для архівації потрібно добавити файли та назвати архів!");
+        }
+
+        private void unzipToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            CommonOpenFileDialog commonOpenFileDialog = new CommonOpenFileDialog();
+            commonOpenFileDialog.IsFolderPicker = true;
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = false;
+            openFileDialog.Filter = "Zip Files|*.zip;*.rar;*.gzar";
+
+            if (openFileDialog.ShowDialog() == DialogResult.Cancel)
+                return;
+
+            if (commonOpenFileDialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                using (ZipArchive zip = ZipFile.Open(openFileDialog.FileName, ZipArchiveMode.Read))
+                {
+                    zip.ExtractToDirectory(commonOpenFileDialog.FileName);
+                }
+                MessageBox.Show("Успішно розархівовано в: " + commonOpenFileDialog.FileName);
+            }
+        }
+    }
+}
